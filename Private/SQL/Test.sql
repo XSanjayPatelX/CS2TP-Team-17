f1bcfbf344a95a562557e7795b936402b43f3a6b
Sanjay Patel <Sanjay-_-Patel@outlook.com>
2022-11-14 12:59:50 +0000

    Added folder for SQL files to be added in

---
+++ Private/SQL/Test.sql
@@ -0,0 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 09, 2022 at 11:57 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
