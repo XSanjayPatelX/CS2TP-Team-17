6ee91d612736e17961570514c0a6e878f986538d
hansraj2002 <116090037+hansraj2002@users.noreply.github.com>
2022-11-29 15:23:56 +0000

    Create admin.php

---
+++ Public/Front-End/PHP/admin.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Health Care Website</title>
+
+        <link rel="stylesheet" href="../CSS/style.css">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    </head>
+
+    <body>
+        <header>
+
+            <div class="bx bx-menu" id="menu-icon"></div>
+
+            <ul class="navbar">
+                <li><a href="index.php#shopContainer">Products</a></li>
+                <li><a href="account.php">Account</a></li>
+            </ul>
+
+            <div class="header-btn">
+
+                <a href="signIn.php" class="sign-in" id="sign-in">Sign In</a>
+                <a href="">Logout</a>
+                <a href="cart.php">Cart</a>
+            </div>
+        </header>
+
+        </section>
+
+        <section class="shopContainer" id="shopContainer">
+            <h2 class="section-title">Shop Products</h2>
+
+    <div class="shop-content">
+        <div class="product-box">
+            <img src="../E-Commerce-Designs/Products/P1.PNG" alt="" class="product-img">
+            <h2 class="product-title">Daily skin care kit</h2>
+            <span class="price">£45</span>
+            <i class='bx bx-shopping-bag add-cart'></i>
+        </div>
+
+        <div class="product-box">
+            <img src="../E-Commerce-Designs/Products/P2.PNG" alt="" class="product-img">
+            <h2 class="product-title">Anti wrinkle cream</h2>
+            <span class="price">£10</span>
+            <i class='bx bx-shopping-bag add-cart'></i>
+        </div>
+
+        <div class="product-box">
+            <img src="../E-Commerce-Designs/Products/P3.PNG" alt="" class="product-img">
+            <h2 class="product-title">Insulin pump</h2>
+            <span class="price">£20</span>
+            <i class='bx bx-shopping-bag add-cart'></i>
+        </div>
+
+        <div class="product-box">
+            <img src="../E-Commerce-Designs/Products/P4.PNG" alt="" class="product-img">
+            <h2 class="product-title">After shave</h2>
+            <span class="price">£14</span>
+            <i class='bx bx-shopping-bag add-cart'></i>
+        </div>
+
+        <div class="product-box">
+            <img src="../E-Commerce-Designs/Products/P5.PNG" alt="" class="product-img">
+            <h2 class="product-title">Bottle of shampoo</h2>
+            <span class="price">£6</span>
+            <i class='bx bx-shopping-bag add-cart'></i>
+        </div>
+
+        <div class="product-box">
+            <img src="../E-Commerce-Designs/Products/P6.PNG" alt="" class="product-img">
+            <h2 class="product-title">Perfume</h2>
+            <span class="price">£25</span>
+            <i class='bx bx-shopping-bag add-cart'></i>
+        </div>
+
+        <div class="product-box">
+            <img src="../E-Commerce-Designs/Products/P7.PNG" alt="" class="product-img">
+            <h2 class="product-title">Thermometer</h2>
+            <span class="price">£12</span>
+            <i class='bx bx-shopping-bag add-cart'></i>
+        </div>
+
+        <div class="product-box">
+            <img src="../E-Commerce-Designs/Products/P8.PNG" alt="" class="product-img">
+            <h2 class="product-title">White face mask</h2>
+            <span class="price">£1</span>
+            <i class='bx bx-shopping-bag add-cart'></i>
+        </div>       
+    </div>
+</section>
+        <script src="../JS/main.js"></script>
+
+    </body>
+</html>
