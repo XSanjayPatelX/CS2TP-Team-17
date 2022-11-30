005c76c9251b3e6b7cd0d8a1fe95c664ec9c3fde
hansraj2002 <116090037+hansraj2002@users.noreply.github.com>
2022-11-30 16:15:10 +0000

    Update admin.php

--- Public/Front-End/PHP/admin.php
+++ Public/Front-End/PHP/admin.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Health Care Website</title>
+        <title>Admin Page</title>
 
         <link rel="stylesheet" href="../CSS/style.css">
 
@@ -17,44 +17,78 @@
 
             <div class="bx bx-menu" id="menu-icon"></div>
 
-            <ul class="navbar">             
-                <li><a href="index.php#shopContainer">Products</a></li>
-                <li><a href="account.php">Account</a></li>
-            </ul>
+            <ul class="navbar">
 
-            <div class="header-btn">
-                <a href="signIn.php" class="sign-in" id="sign-in">Sign In</a>
-                <a href="cart.php">Cart</a>
-            </div>
+                <li><a href="index.php#shopContainer">Products Stock</a></li>
+            </ul>
         </header>
 
         <section class="shopContainer" id="shopContainer">
-            <h2 class="section-title">List Of Stocked Products</h2>
-
+            <h2 class="section-title">List of Stocked Products</h2>
             <div class="shop-content">
-                <?php
-                $sql = "SELECT product, product_description, size, price, images FROM products";
-                $result = $conn->query($sql);
-
-                while($row = $result->fetch_assoc()) { ?>
-                    <div class="product-box">
-                        <table style="width: 100%;">
-                            <div class="input-box">
-                                    <tr>
-                                        <th><img src="../E-Commerce-Designs/Products/<?php echo $row["images"]; ?>" class="product-img"></th>
-                                    </tr>
-                                    <tr>
-                                        <th><h2 class="product-title-price"><?php echo $row["product"] . " - £" . $row["price"]; ?></h2></th>
-                                    </tr>
-                                    <tr>
-                                        <th><h2 class="product-size">Size/Amount: <?php echo $row["size"]; ?></h2></th>
-                                    </tr>
-                                </div>
-                        </table>
+                <div class="product-box">
+                    <div class="product-img">
+                        <img src="../E-Commerce-Designs/Products/P1.PNG" alt="">
                     </div>
-                <?php } ?>
+                    <p>Health Care Kit</p>
+                    <h2> Quantity: <span>100</span></h2>
+                </div>
+
+                <div class="product-box">
+                    <div class="product-img">
+                        <img src="../E-Commerce-Designs/Products/P2.PNG" alt="">
+                    </div>
+                    <p>Anti Wrinkle Cream</p>
+                    <h2> Quantity: <span>150</span></h2>
+                </div>
+
+                <div class="product-box">
+                    <div class="product-img">
+                        <img src="../E-Commerce-Designs/Products/P3.PNG" alt="">
+                    </div>
+                    <p>Insulin pump</p>
+                    <h2> Quantity: <span>80</span></h2>
+                </div>
+
+                <div class="product-box">
+                    <div class="product-img">
+                        <img src="../E-Commerce-Designs/Products/P4.PNG" alt="">
+                    </div>
+                    <p>After Shave</p>
+                    <h2> Quantity: <span>120</span></h2>
+                </div>
+
+                <div class="product-box">
+                    <div class="product-img">
+                        <img src="../E-Commerce-Designs/Products/P5.PNG" alt="">
+                    </div>
+                    <p>Bottle of Shampoo</p>
+                    <h2> Quantity: <span>200</span></h2>
+                </div>
+
+                <div class="product-box">
+                    <div class="product-img">
+                        <img src="../E-Commerce-Designs/Products/P6.jpg" alt="">
+                    </div>
+                    <p>Perfume</p>
+                    <h2> Quantity: <span>180</span></h2>
+                </div>
+
+                <div class="product-box">
+                    <div class="product-img">
+                        <img src="../E-Commerce-Designs/Products/P7.PNG" alt="">
+                    </div>
+                    <p>Thermometer</p>
+                    <h2> Quantity: <span>300</span></h2>
+                </div>
+
+                <div class="product-box">
+                    <div class="product-img">
+                        <img src="../E-Commerce-Designs/Products/P8.PNG" alt="">
+                    </div>
+                    <p>White Face Mask</p>
+                    <h2> Quantity: <span>1000</span></h2>
+                </div>
+
             </div>
         </section>
-
-    </body>
-</html>
